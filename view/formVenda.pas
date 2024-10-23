c2a1bd5b56ac7a754e171cca1b5dd9f8af95309d
BRUNO PIGA <81204561+B-Piga@users.noreply.github.com>
2024-10-23 17:55:22 -0300

    .

--- view/formVenda.pas
+++ view/formVenda.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
+  Vcl.Imaging.pngimage;
 
 type
   TvendaForm = class(TForm)
@@ -12,6 +13,18 @@ type
     pnlPrincipal: TPanel;
     pnlSair: TPanel;
     pnlFooter: TPanel;
+    Panel1: TPanel;
+    Label1: TLabel;
+    Panel2: TPanel;
+    Label2: TLabel;
+    Panel3: TPanel;
+    imgCliente: TImage;
+    Panel4: TPanel;
+    Label3: TLabel;
+    lblCliente: TLabel;
+    Panel5: TPanel;
+    Image2: TImage;
+    Label4: TLabel;
     procedure pnlSairClick(Sender: TObject);
     procedure pnlSairMouseEnter(Sender: TObject);
     procedure pnlSairMouseLeave(Sender: TObject);
